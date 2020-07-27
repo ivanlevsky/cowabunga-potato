97f8ae1be0c1eb0197efa3325c91f1c2cc719315
ivanlevsky <ivanlevsky@gmail.com>
2020-07-27 18:13:07 +0800

    add test folders path

--- python_common/global_param.py
+++ python_common/global_param.py
@@ -1,12 +1,17 @@
 from python_common.file_and_system.config_utils import *
 import os
 
-
 project_path = os.path.dirname(os.getcwd())
 conf_path = ''.join((project_path + r'\test file\cf.properties'))
+section_test_path = 'test_path'
 section_opencv_utils = 'opencv_utils'
 section_machine_learning = 'machine_learning'
 section_appium = 'appium'
+
+# test_path section
+test_image_path = ''.join((project_path,read_conf_file(conf_path,section_test_path,'test_image_path')[2]))
+test_video_path = ''.join((project_path,read_conf_file(conf_path,section_test_path,'test_video_path')[2]))
+test_file_path = ''.join((project_path,read_conf_file(conf_path,section_test_path,'test_file_path')[2]))
 
 # opencv_utils section
 system_font_path = read_conf_file(conf_path,section_opencv_utils,'system_font_path')[2]
@@ -27,19 +32,15 @@ appium_screenshot_path = ''.join((project_path,read_conf_file(conf_path,section_
 appium_screenrecord_path = ''.join((project_path,read_conf_file(conf_path,section_appium,'appium_screenrecord_path')[2]))
 qr_code_image_path = ''.join((project_path,read_conf_file(conf_path,section_appium,'qr_code_image_path')[2]))
 
-
 # machine learning section
 ml_ch2_housing_data = ''.join((
     project_path,read_conf_file(conf_path,section_machine_learning,'ml_ch2_housing_data')[2]))
 ml_ch2_housing_image = ''.join((
     project_path,read_conf_file(conf_path,section_machine_learning,'ml_ch2_housing_image')[2]))
-
 ml_ch3_sklearn_data_home = ''.join((
     project_path,read_conf_file(conf_path,section_machine_learning,'ml_ch3_sklearn_data_home')[2]))
-
 ml_numpy_array_save_path = ''.join((
     project_path,read_conf_file(conf_path,section_machine_learning,'ml_numpy_array_save_path')[2]))
-
 ml_matplotlib_figure_save_path = ''.join((
     project_path,read_conf_file(conf_path,section_machine_learning,'ml_matplotlib_figure_save_path')[2]))
 
