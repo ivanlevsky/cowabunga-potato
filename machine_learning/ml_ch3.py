e615b2dc7446bd23c1f22ffccd2e1f251f2c9dcf
ivanlevsky <ivanlevsky@gmail.com>
2020-08-04 15:20:10 +0800

    machine learning chapter 3 practice

---
+++ machine_learning/ml_ch3.py
@@ -0,0 +1,121 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_predict, cross_val_score
+from sklearn.metrics import confusion_matrix, precision_recall_curve, roc_curve
+from sklearn.preprocessing import StandardScaler
+from python_common.global_param import ml_ch3_sklearn_data_home
+from sklearn.datasets import fetch_openml
+from sklearn.linear_model import SGDClassifier
+
+def plot_digits(instances, images_per_row=5, **options):
+    size = 28
+    images_per_row = min(len(instances), images_per_row)
+    images = [instance.reshape(size,size) for instance in instances]
+    n_rows = (len(instances) - 1) // images_per_row + 1
+    row_images = []
+    n_empty = n_rows * images_per_row - len(instances)
+    images.append(np.zeros((size, size * n_empty)))
+    for row in range(n_rows):
+        rimages = images[row * images_per_row : (row + 1) * images_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    image = np.concatenate(row_images, axis=0)
+    plt.imshow(image, cmap=plt.cm.binary, **options)
+    plt.axis("off")
+
+
+def sort_by_target(mnist):
+    reorder_train = np.array(sorted([(target, i) for i, target in enumerate(mnist.target[:60000])]))[:, 1]
+    reorder_test = np.array(sorted([(target, i) for i, target in enumerate(mnist.target[60000:])]))[:, 1]
+    mnist.data[:60000] = mnist.data[reorder_train]
+    mnist.target[:60000] = mnist.target[reorder_train]
+    mnist.data[60000:] = mnist.data[reorder_test + 60000]
+    mnist.target[60000:] = mnist.target[reorder_test + 60000]
+
+
+mnist = fetch_openml('mnist_784', data_home=ml_ch3_sklearn_data_home, version=1, cache=True)
+mnist.target = mnist.target.astype(np.int8)
+sort_by_target(mnist)
+
+X, y = mnist["data"], mnist["target"]
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+shuffle_index = np.random.permutation(60000)
+X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test == 5)
+sgd_clf = SGDClassifier(max_iter=5, tol=-np.infty, random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+some_digit = X[36000]
+# y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+# confusion_matrix(y_train_5, y_train_pred)
+# y_train_perfect_predictions = y_train_5
+# confusion_matrix(y_train_5, y_train_perfect_predictions)
+
+# y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3,
+#                              method="decision_function")
+# precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+#
+# def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+#     plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
+#     plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
+#     plt.xlabel("Threshold")
+#     plt.legend(loc="upper left")
+#     plt.ylim([0, 1])
+#
+# plt.figure(figsize=(8, 4))
+# plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+# plt.xlim([-700000, 700000])
+# plt.show()
+
+# fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+
+
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.axis([0, 1, 0, 1])
+    plt.xlabel('False Positive Rate', fontsize=16)
+    plt.ylabel('True Positive Rate', fontsize=16)
+# plt.figure(figsize=(8, 6))
+# plot_roc_curve(fpr, tpr)
+# plt.show()
+
+
+# forest_clf = RandomForestClassifier(n_estimators=10, random_state=42)
+# y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
+#                                     method="predict_proba")
+
+# y_scores_forest = y_probas_forest[:, 1] # score = proba of positive class
+# fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_5,y_scores_forest)
+# plt.figure(figsize=(8, 6))
+# plt.plot(fpr, tpr, "b:", linewidth=2, label="SGD")
+# plot_roc_curve(fpr_forest, tpr_forest, "Random Forest")
+# plt.legend(loc="lower right", fontsize=16)
+# plt.show()
+
+# -------------------------Error Analysis-------------------------
+# scaler = StandardScaler()
+# X_train_scaled = scaler.fit_transform(X_train.astype(np.float64))
+# cross_val_score(sgd_clf, X_train_scaled, y_train, cv=3, scoring="accuracy")
+# y_train_pred = cross_val_predict(sgd_clf, X_train_scaled, y_train, cv=3)
+# conf_mx = confusion_matrix(y_train, y_train_pred)
+# plt.matshow(conf_mx, cmap=plt.cm.gray)
+# plt.show()
+# row_sums = conf_mx.sum(axis=1, keepdims=True)
+# norm_conf_mx = conf_mx / row_sums
+# np.fill_diagonal(norm_conf_mx, 0)
+# plt.matshow(norm_conf_mx, cmap=plt.cm.gray)
+# plt.show()
+
+# cl_a, cl_b = 8, 9  # 3, 5
+# X_aa = X_train[(y_train == cl_a) & (y_train_pred == cl_a)]
+# X_ab = X_train[(y_train == cl_a) & (y_train_pred == cl_b)]
+# X_ba = X_train[(y_train == cl_b) & (y_train_pred == cl_a)]
+# X_bb = X_train[(y_train == cl_b) & (y_train_pred == cl_b)]
+# plt.figure(figsize=(8,8))
+# plt.subplot(221); plot_digits(X_aa[:25], images_per_row=5)
+# plt.subplot(222); plot_digits(X_ab[:25], images_per_row=5)
+# plt.subplot(223); plot_digits(X_ba[:25], images_per_row=5)
+# plt.subplot(224); plot_digits(X_bb[:25], images_per_row=5)
+# plt.show()
