1c12a3d86c30895246183d417938c8a59a068fd3
ivanlevsky <ivanlevsky@gmail.com>
2020-10-29 17:22:27 +0800

    add database test code

---
+++ databases/database_test.py
@@ -0,0 +1,12 @@
+from databases.database_utils import connect_to_databases, execute_sql
+from python_common.global_param import mariadb_url,mariadb_user,mariadb_password
+from python_common.global_param import pgsql_url,pgsql_user,pgsql_password
+
+
+sql = 'select * from student'
+maria_connection = connect_to_databases(mariadb_url,mariadb_user, mariadb_password)
+execute_sql(maria_connection, sql, True)
+
+postgresql_connection = connect_to_databases(pgsql_url, pgsql_user, pgsql_password)
+execute_sql(postgresql_connection,  sql, True)
+
