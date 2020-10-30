f061efcd0d243fa8bfaf3f0299affdc0010c8872
ivanlevsky <ivanlevsky@gmail.com>
2020-10-30 10:26:01 +0800

    rename module databases to datasets

--- datasets/database_test.py
+++ datasets/database_test.py
@@ -1,4 +1,4 @@
-from databases.database_utils import connect_to_databases, execute_sql
+from datasets.database_utils import connect_to_databases, execute_sql
 from python_common.global_param import mariadb_url,mariadb_user,mariadb_password
 from python_common.global_param import pgsql_url,pgsql_user,pgsql_password
 
