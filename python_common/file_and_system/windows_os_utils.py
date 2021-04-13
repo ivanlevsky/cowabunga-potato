bc3042b8a602b2bec7349bd342fb33b5144e84e3
ivanlevsky <ivanlevsky@gmail.com>
2021-04-13 10:42:18 +0800

    add run_process_by_name, check_process_running functions

--- python_common/file_and_system/windows_os_utils.py
+++ python_common/file_and_system/windows_os_utils.py
@@ -5,6 +5,26 @@ def kill_process_by_name(name):
     os.system('taskkill /f /im  ' + name)
 
 
+def run_process_by_name(name):
+    os.system(name)
+
+
+def check_process_running(name):
+    running = False
+    if name.__contains__('\\'):
+        name = name[name.rfind('\\'):].replace('\\','')
+    elif name.__contains__('/'):
+        name = name[name.rfind('/'):].replace('/','')
+    elif name.__contains__('//'):
+        name = name[name.rfind('//'):].replace('//','')
+    (status, output) = subprocess.getstatusoutput(''.join(('tasklist /nh /fi  \"Imagename eq ', name, '\"')))
+
+    if status == 0:
+        if output.strip().startswith(name):
+            running = True
+        return running
+
+
 def get_command_output(cmd):
     (status, output) = subprocess.getstatusoutput(cmd)
     if status == 0:
@@ -14,4 +34,6 @@ def get_command_output(cmd):
 def get_shell_output(shell_cmd, execute_cmd):
     shell_cmd.append(execute_cmd)
     return subprocess.Popen(shell_cmd, stdout=subprocess.PIPE).communicate()[0].decode('utf8')
+
+
 
