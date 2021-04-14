34f396b1b90604b57c1f91e0a07142c029463bdd
ivanlevsky <ivanlevsky@gmail.com>
2021-04-14 11:06:41 +0800

    android_os_util fix windows_os_utils class, add check opened app with "mSurface" info

--- python_common/file_and_system/android_os_utils.py
+++ python_common/file_and_system/android_os_utils.py
@@ -1,29 +1,41 @@
-from python_common.file_and_system.windows_os_utils import *
+from python_common.file_and_system.windows_os_utils import WindowsOsUtil
 from python_common.string_utils import *
 from python_common.global_param import aapt_path,android_apk_list
 from python_common.file_and_system.file_utils import *
 
 
 def android_device_list():
-    return get_command_output('adb devices').split('\n')[1].replace('device', '').strip()
+    return WindowsOsUtil.get_command_output('adb devices').split('\n')[1].replace('device', '').strip()
 
 
 def android_version():
-    return get_command_output('adb shell getprop ro.build.version.release')
+    return WindowsOsUtil.get_command_output('adb shell getprop ro.build.version.release')
 
 
 def android_api_version():
-    return get_command_output('adb shell getprop ro.build.version.sdk')
+    return WindowsOsUtil.get_command_output('adb shell getprop ro.build.version.sdk')
 
 
 def android_current_opened_app_info():
-    msg = get_shell_output(['adb', 'shell'], 'dumpsys window windows | grep -E \'mCurrentFocus|mFocusedApp\'')
-    msg = split_string_by_regex('{|}', msg)[1].split(' ')[2]
-    return msg.split('/')[0], msg.split('/')[1]
+    adb_dumpsys_window_msg = WindowsOsUtil.get_shell_output(['adb', 'shell'], 'dumpsys window windows')
+    msg_split_one = ''
+    msg_split_two = ''
+    if adb_dumpsys_window_msg.__contains__('mCurrentFocus'):
+        msg = WindowsOsUtil.get_shell_output(['adb', 'shell'], 'dumpsys window windows | grep -E \'mCurrentFocus|mFocusedApp\'')
+        msg = split_string_by_regex('{|}', msg)[1].split(' ')[2]
+        msg_split_one = msg.split('/')[0]
+        msg_split_two = msg.split('/')[1]
+    elif adb_dumpsys_window_msg.__contains__('mSurface'):
+        msg = WindowsOsUtil.get_shell_output(['adb', 'shell'], 'dumpsys window windows | grep -E \'mSurface=Surface\'')
+        msg = re.findall('mSurface=Surface\\(name=' + '(.+?)'+'/'+'(.+?)'+'\)/@',msg)
+        msg_split_one = msg.__getitem__(0)[0]
+        msg_split_two = msg.__getitem__(0)[1]
+
+    return msg_split_one, msg_split_two
 
 
 def android_check_app_active(app_package):
-    msg = get_shell_output(['adb', 'shell'], 'dumpsys window windows | grep -e \'Window #\'').split('\r\n')
+    msg = WindowsOsUtil.get_shell_output(['adb', 'shell'], 'dumpsys window windows | grep -e \'Window #\'').split('\r\n')
     for m in msg:
         if m.__contains__(app_package):
             msg = split_string_by_regex('{|}', m.strip())[1].split(' ')[2]
@@ -32,17 +44,17 @@ def android_check_app_active(app_package):
 
 
 def android_aapt_install():
-    print(get_command_output(' '.join(('adb push', aapt_path, '/data/local/tmp'))))
-    print(get_command_output('adb shell chmod 0755 /data/local/tmp/aapt-arm-pie'))
+    print(WindowsOsUtil.get_command_output(' '.join(('adb push', aapt_path, '/data/local/tmp'))))
+    print(WindowsOsUtil.get_command_output('adb shell chmod 0755 /data/local/tmp/aapt-arm-pie'))
 
 
 
 def android_home_button():
-    get_shell_output(['adb', 'shell'], 'am start -W -c android.intent.category.HOME -a android.intent.action.MAIN')
+    WindowsOsUtil.get_shell_output(['adb', 'shell'], 'am start -W -c android.intent.category.HOME -a android.intent.action.MAIN')
 
 
 def android_all_package_list():
-    msg = filter(None, get_shell_output(['adb', 'shell'], 'pm list packages -3 -f').split('\r\n'))
+    msg = filter(None, WindowsOsUtil.get_shell_output(['adb', 'shell'], 'pm list packages -3 -f').split('\r\n'))
     package_list = []
     for ms in msg:
         ms = android_aapt_get_app_info(ms[ms.index('/'):ms.rindex('=')]).replace('\r\n', '').split('\'')
@@ -71,13 +83,13 @@ def android_all_package_list():
 def android_aapt_get_app_info(app_path):
     cmd = ' '.join(('/data/local/tmp/aapt-arm-pie d badging', app_path, '|grep -E',
                     '\'package|launchable-activity|application-label\''))
-    msg = (get_shell_output(['adb', 'shell'], cmd))
+    msg = (WindowsOsUtil.get_shell_output(['adb', 'shell'], cmd))
     return msg
 
 
 def android_search_app_activity(app_package):
     cmd = ' '.join(('cmd package resolve-activity', app_package, '|grep name'))
-    msg = (get_shell_output(['adb', 'shell'], cmd))
+    msg = (WindowsOsUtil.get_shell_output(['adb', 'shell'], cmd))
     if msg == '':
         msg = 'no activity info'
     else:
@@ -94,7 +106,7 @@ def android_search_package_by_name(app_name):
 
 def android_check_screen_is_locked():
     cmd = 'dumpsys window | grep mDreamingLockscreen'
-    msg = (get_shell_output(['adb', 'shell'], cmd))
+    msg = (WindowsOsUtil.get_shell_output(['adb', 'shell'], cmd))
     return msg.__contains__('mDreamingLockscreen=true')
 
 
@@ -104,15 +116,13 @@ def android_check_screen_is_locked():
 
 def android_find_app_user_id(app_package):
     cmd = ''.join(('dumpsys package ',app_package,' | grep userId'))
-    msg = get_shell_output(['adb', 'shell'], cmd)
+    msg = WindowsOsUtil.get_shell_output(['adb', 'shell'], cmd)
     return msg
 
 
 def android_backup_app_apk(app_package, backup_path):
     cmd = ''.join(('pm path ',app_package))
-    msg = get_shell_output(['adb', 'shell'], cmd)
+    msg = WindowsOsUtil.get_shell_output(['adb', 'shell'], cmd)
     cmd = ''.join(('adb pull ', msg.replace('package:','').strip(),' ', backup_path))
-    get_command_output(cmd)
-
-
+    WindowsOsUtil.get_command_output(cmd)
 
