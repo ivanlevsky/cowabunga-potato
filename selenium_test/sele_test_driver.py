1b131860d03649bf487017edca0339df8bd8d7d8
ivanlevsky <ivanlevsky@gmail.com>
2021-04-14 11:08:05 +0800

    fix windows_os_util class

--- selenium_test/sele_test_driver.py
+++ selenium_test/sele_test_driver.py
@@ -1,10 +1,10 @@
-from python_common.file_and_system.windows_os_utils import kill_process_by_name
+from python_common.file_and_system.windows_os_utils import WindowsOsUtil
 from selenium_test.selenium_utils import init_driver, open_browser_single_tab
 from python_common.global_param import ie_driver_path,chrome_driver_path,edge_driver_path
 
-kill_process_by_name('MicrosoftWebDriver.exe')
-kill_process_by_name('chromedriver.exe')
-kill_process_by_name('IEDriverServer.exe')
+WindowsOsUtil.kill_process_by_name('MicrosoftWebDriver.exe')
+WindowsOsUtil.kill_process_by_name('chromedriver.exe')
+WindowsOsUtil.kill_process_by_name('IEDriverServer.exe')
 chrome_driver = init_driver('chrome', chrome_driver_path)
 open_browser_single_tab(chrome_driver,'https://www.baidu.com')
 chrome_driver.quit()
