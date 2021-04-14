b95d2ac444126b26c583751c70874a03a41ed3d3
ivanlevsky <ivanlevsky@gmail.com>
2021-04-14 11:04:49 +0800

    update windows_os_utils to class

--- python_common/file_and_system/windows_os_utils.py
+++ python_common/file_and_system/windows_os_utils.py
@@ -1,39 +1,38 @@
 import os, subprocess
 
 
-def kill_process_by_name(name):
-    os.system('taskkill /f /im  ' + name)
+class WindowsOsUtil:
+    @staticmethod
+    def kill_process_by_name(name):
+        os.system('taskkill /f /im  ' + name)
 
+    @staticmethod
+    def run_process_by_name(name):
+        subprocess.Popen(name)
 
-def run_process_by_name(name):
-    subprocess.Popen(name)
+    @staticmethod
+    def check_process_running(name):
+        running = False
+        if name.__contains__('\\'):
+            name = name[name.rfind('\\'):].replace('\\', '')
+        elif name.__contains__('/'):
+            name = name[name.rfind('/'):].replace('/', '')
+        elif name.__contains__('//'):
+            name = name[name.rfind('//'):].replace('//', '')
+        (status, output) = subprocess.getstatusoutput(''.join(('tasklist /nh /fi  \"Imagename eq ', name, '\"')))
 
+        if status == 0:
+            if output.strip().startswith(name):
+                running = True
+            return running
 
-def check_process_running(name):
-    running = False
-    if name.__contains__('\\'):
-        name = name[name.rfind('\\'):].replace('\\','')
-    elif name.__contains__('/'):
-        name = name[name.rfind('/'):].replace('/','')
-    elif name.__contains__('//'):
-        name = name[name.rfind('//'):].replace('//','')
-    (status, output) = subprocess.getstatusoutput(''.join(('tasklist /nh /fi  \"Imagename eq ', name, '\"')))
+    @staticmethod
+    def get_command_output(cmd):
+        (status, output) = subprocess.getstatusoutput(cmd)
+        if status == 0:
+            return output
 
-    if status == 0:
-        if output.strip().startswith(name):
-            running = True
-        return running
-
-
-def get_command_output(cmd):
-    (status, output) = subprocess.getstatusoutput(cmd)
-    if status == 0:
-        return output
-
-
-def get_shell_output(shell_cmd, execute_cmd):
-    shell_cmd.append(execute_cmd)
-    return subprocess.Popen(shell_cmd, stdout=subprocess.PIPE).communicate()[0].decode('utf8')
-
-
-
+    @staticmethod
+    def get_shell_output(shell_cmd, execute_cmd):
+        shell_cmd.append(execute_cmd)
+        return subprocess.Popen(shell_cmd, stdout=subprocess.PIPE).communicate()[0].decode('utf8')
