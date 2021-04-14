e9cf81a805e9c7c63bfe2f181a08270bc82dcaea
ivanlevsky <ivanlevsky@gmail.com>
2021-04-14 16:16:45 +0800

    add test_windows_os_utils unittest

---
+++ python_unittest/test_windows_os_utils.py
@@ -0,0 +1,44 @@
+import unittest,warnings
+from python_common.file_and_system.windows_os_utils import WindowsOsUtil
+
+
+class TestWindowsOsUtil(unittest.TestCase):
+    processIsRunning = ''
+    processNotRunning = ''
+    processToTestRunAndKill = ''
+    command = ''
+
+    @classmethod
+    def setUpClass(cls):
+        cls.processIsRunning = 'python.exe'
+        cls.processNotRunning = 'retroarch.exe'
+        cls.processToTestRunAndKill = 'notepad.exe'
+        cls.command = 'echo 123'
+
+    def tearDown(self):
+        if WindowsOsUtil.check_process_running(self.processToTestRunAndKill):
+            WindowsOsUtil.kill_process_by_name(self.processToTestRunAndKill)
+
+    def test_kill_and_run_process_by_name(self):
+        warnings.simplefilter('ignore', ResourceWarning)
+        WindowsOsUtil.run_process_by_name('notepad.exe')
+        self.assertTrue(WindowsOsUtil.check_process_running('notepad.exe'))
+
+        WindowsOsUtil.kill_process_by_name('notepad.exe')
+        self.assertFalse(WindowsOsUtil.check_process_running('notepad.exe'))
+
+    def test_check_process_running(self):
+        with self.subTest():
+            self.assertTrue(WindowsOsUtil.check_process_running(self.processIsRunning))
+        with self.subTest():
+            self.assertFalse(WindowsOsUtil.check_process_running(self.processNotRunning))
+
+    def test_get_command_output(self):
+        self.assertEqual('123', WindowsOsUtil.get_command_output(self.command))
+
+    def test_get_shell_output(self):
+        self.assertEqual('123', WindowsOsUtil.get_shell_output(['powershell'], self.command))
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
