76c9c54d05ab05c207496b3c10b7ae99acfd0d76
ivanlevsky <ivanlevsky@gmail.com>
2021-05-18 18:27:30 +0800

    fix gif path sort, covert image_utils to class

--- graphic/image_utils.py
+++ graphic/image_utils.py
@@ -11,25 +11,36 @@ from PIL import Image, GifImagePlugin
 #         os.mkdir(image_path)
 #     with open(image_path, 'wb') as f:
 #         f.write(base64.b64decode(base64Value))
+class ImageUtils():
+    # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html
+    @staticmethod
+    def combine_images_to_gif(images_path, image_format, gif_path):
+        sorted_base_path = images_path
+        sorted_images_list = []
+        images_path += ''.join(('*.', image_format))
+        images_list = []
+        for f in glob.glob(images_path):
+            images_list.append(f[f.rfind('\\'):].replace('\\', '').replace('.png',''))
 
+        for ff in sorted(images_list,key=int):
+            sorted_images_list.append(''.join((sorted_base_path, ff, '.', image_format)))
 
-# https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html
-def combine_images_to_gif(images_path, gif_path):
-    img, *imgs = [Image.open(f) for f in sorted(glob.glob(images_path))]
-    img.save(fp=gif_path, format='GIF', append_images=imgs,
-             save_all=True, duration=100, loop=0,include_color_table=True)
+        img, *imgs = [Image.open(f) for f in sorted_images_list]
+        img.save(fp=gif_path, format='GIF', append_images=imgs,
+                 save_all=True, duration=100, loop=0,include_color_table=True)
 
+    @staticmethod
+    def split_gif_to_images(gif_path):
+        image_object = Image.open(gif_path)
+        if image_object.is_animated:
+            for frame in range(0,image_object.n_frames):
+                image_object.seek(frame)
+                image_object.save(fp=GlobalParam.get_gif_import() + str(frame) +'.png', format='PNG')
 
-def split_gif_to_images(gif_path):
-    imageObject = Image.open(gif_path)
-    if imageObject.is_animated:
-        for frame in range(0,imageObject.n_frames):
-            imageObject.seek(frame)
-            imageObject.save(fp=GlobalParam.get_gif_import() + str(frame) +'-.png', format='PNG')
 
 # fp_out = GlobalParam.get_gif_export() + 'out.gif'
 # split_gif_to_images(fp_out)
 
-fp_in = GlobalParam.get_gif_import() + '*.png'
-fp_out = GlobalParam.get_gif_export() + 'out.gif'
-combine_images_to_gif(fp_in, fp_out)
+# fp_in = GlobalParam.get_gif_import()
+# fp_out = GlobalParam.get_gif_export() + 'out.gif'
+# ImageUtils.combine_images_to_gif(fp_in, 'png', fp_out)
