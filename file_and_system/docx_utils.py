0eb6211a338d1e1824f423fea39f59c51d59c994
ivanlevsky <ivanlevsky@gmail.com>
2021-06-28 11:32:44 +0800

    docx utils add read docx functions

--- file_and_system/docx_utils.py
+++ file_and_system/docx_utils.py
@@ -4,12 +4,14 @@ from docx.enum.style import WD_STYLE_TYPE
 from docx.enum.text import WD_BREAK
 from docx.shared import Inches
 from python_common.global_param import GlobalParam
+import re
 
 document = Document()
 
 
 def add_document_text(text, **option):
     paragraph = document.add_paragraph('')
+    default_font_name = 'Arial'
     default_font_size = 15
     default_font_bold = False
     default_font_italic = False
@@ -21,7 +23,7 @@ def add_document_text(text, **option):
         default_font_italic = option.get('font_italic')
     runner = paragraph.add_run(text)
     runner.bold = default_font_bold
-    # runner.font.name = 'Arial'
+    runner.font.name = default_font_name
     runner.italic = default_font_italic
     runner.font.size = Pt(default_font_size)
     runner.add_break(WD_BREAK.LINE)
@@ -73,8 +75,53 @@ def docx_table_style_example():
 def save_docx_file(file_name):
     document.save(GlobalParam.get_word_report() + file_name)
 
+# convert text and keywords to list in order
+def reformat_text_and_keyword(text):
+    re_text = []
+    split_text = list(filter(None, re.split('(\{[A-Za-z0-9_-]*\([A-Za-z0-9_-]*\)\})', text)))
+    for st in split_text:
+        keyword_and_params = list(filter(None, re.findall('\{[A-Za-z0-9_-]*\([A-Za-z0-9_-]*\)\}', st)))
+        if keyword_and_params.__len__() > 0:
+            for kp in keyword_and_params:
+                kpl = list(filter(None, re.split('[{()}]', kp)))
+                if kpl.__len__() > 0:
+                    re_text.append(''.join((kpl[0],'_s_p_l_i_t_',kpl[1])))
+                else:
+                    re_text.append(st)
+        else:
+            re_text.append(st)
+    return re_text
 
-docx_table_style_example()
-save_docx_file('demo.docx')
+
+# read doc text data and trigger keyword functions
+def read_docx_file(file_name):
+    read_document = Document(GlobalParam.get_word_report() + file_name)
+    for doc_para in read_document.paragraphs:
+        paragraph = document.add_paragraph('', style=doc_para.style)
+        # print('----------'+doc_para.text)
+        # print(reformat_text_and_keyword(doc_para.text))
+        for run in doc_para.runs:
+            # print(run.font.color.rgb)
+            print(run.text)
+            # keyword_function(doc_para.text)
+            # paragraph.add_run(run.text, style=run.style)
+            # paragraph.add_run(run.text.replace(run.text, '123'), style=run.style)
+            # newrun.font.style = run.font.style
+            # newrun.style = run.style
+        # print(doc_para.text)
+
+        # add_document_text(doc_para.text, font_size = 10, font_bold= False, font_italic = True)
+        # import_db_table_to_docx_table(doc_para.text)
+
+
+# docx_table_style_example()
+# save_docx_file('demo.docx')
+read_docx_file('demo2.docx')
+# save_docx_file('demo2.docx')
+
+
+
+
+
 
 
