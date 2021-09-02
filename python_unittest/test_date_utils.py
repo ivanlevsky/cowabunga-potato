212a16202a3811d9805666bc4c6d95507b01b27b
ivanlevsky <ivanlevsky@gmail.com>
2021-09-02 14:32:20 +0800

    complete date utils test codes

--- python_unittest/test_date_utils.py
+++ python_unittest/test_date_utils.py
@@ -14,7 +14,17 @@ class TestStringUtils(unittest.TestCase):
 
     def test_split_string_by_regex(self):
         with self.subTest():
-            self.assertEqual('2021-09-04', DateUtils.calcu(self.bin_decoded_text))
+            self.assertEqual('2021-09-04', DateUtils.calculate_date(self.old_date, self.calc_date1, False))
+        with self.subTest():
+            self.assertEqual('2021-09-08', DateUtils.calculate_date(self.old_date, self.calc_date1, True))
+        with self.subTest():
+            self.assertEqual('2021-08-15', DateUtils.calculate_date(self.old_date, self.calc_date2, False))
+        with self.subTest():
+            self.assertEqual('2021-08-11', DateUtils.calculate_date(self.old_date, self.calc_date2, True))
+        with self.subTest():
+            self.assertEqual('2020-02-01', DateUtils.get_first_day_of_month(self.first_day_of_month))
+        with self.subTest():
+            self.assertEqual('2021-08-31', DateUtils.get_last_day_of_month(self.last_day_of_month))
 
 if __name__ == '__main__':
     unittest.main()
