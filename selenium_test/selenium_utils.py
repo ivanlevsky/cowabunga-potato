6a3d032cb13788acd6772d767c063e0bae20bca4
ivanlevsky <ivanlevsky@gmail.com>
2021-12-08 18:34:40 +0800

    rename driver_path to driver_option

--- selenium_test/selenium_utils.py
+++ selenium_test/selenium_utils.py
@@ -6,10 +6,10 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
 
 
-def init_driver(browser_type, *driver_path):
+def init_driver(browser_type, *driver_option):
     if browser_type == 'edge':
-        if driver_path.__len__() != 0:
-            webdriver_path = driver_path[0]
+        if driver_option.__len__() != 0:
+            webdriver_path = driver_option[0]
         return webdriver.Edge(executable_path=webdriver_path)
     if browser_type == 'chrome':
         chrome_option = webdriver.ChromeOptions()
@@ -21,16 +21,16 @@ def init_driver(browser_type, *driver_path):
                 'password_manager_enabled': False
             }
         })
-        chrome_option.binary_location = 'D:/Program Files/chromium/chrome.exe'
-        if driver_path.__len__() != 0:
-            webdriver_path = driver_path[0]
+        chrome_option.binary_location = driver_option[1]
+        if driver_option.__len__() != 0:
+            webdriver_path = driver_option[0]
         return webdriver.Chrome(executable_path=webdriver_path, chrome_options=chrome_option)
     if browser_type == 'ie':
         ie_option = webdriver.IeOptions()
         ie_option.add_additional_option(ie_option.IGNORE_ZOOM_LEVEL, True)
         ie_option.add_additional_option(ie_option.IGNORE_PROTECTED_MODE_SETTINGS, True)
-        if driver_path.__len__() != 0:
-            webdriver_path = driver_path[0]
+        if driver_option.__len__() != 0:
+            webdriver_path = driver_option[0]
         return webdriver.Ie(executable_path=webdriver_path, ie_options=ie_option)
 
 
